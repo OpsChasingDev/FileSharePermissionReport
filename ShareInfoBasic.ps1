facb00eabaa291ec258db2fc6c036a2431e27632
drummermanrob20 <robert@stapleton-family.net>
2022-06-16 09:13:31 -0400

    cbh

--- ShareInfoBasic.ps1
+++ ShareInfoBasic.ps1
@@ -1,3 +1,19 @@
+<#
+.SYNOPSIS
+    Retrieves basic information about non-system SMB Shares on a system.
+.DESCRIPTION
+    Retrieves basic information about non-system SMB Shares on a system.
+    The purpose of this function is for use in the FileSharePermissionReport whereby the first step in the process is to identify every non-system SMB share and return only the share name and local path.
+.NOTES
+    Information or caveats about the function e.g. 'This function is not supported in Linux'
+.LINK
+    https://github.com/OpsChasingDev/FileSharePermissionReport
+.EXAMPLE
+    Test-MyTestFunction -Verbose
+    Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
+#>
+
+{0}
 function ShareInfoBasic {
     [OutputType('FSPR.ObjShareInfoBasic')]
     [CmdletBinding()]
