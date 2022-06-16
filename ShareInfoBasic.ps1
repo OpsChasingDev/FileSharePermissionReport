0b9c016f359808bd11f278e5ab8ac4de3119d265
drummermanrob20 <robert@stapleton-family.net>
2022-06-16 08:38:21 -0400

    obj creation with hash

--- ShareInfoBasic.ps1
+++ ShareInfoBasic.ps1
@@ -1,7 +1,8 @@
 $Share = Get-SmbShare | Where-Object {$_.Name -notlike '*$'}
-
+$ColShareInfoBasic = @()
 foreach ($s in $Share) {
-    $ObjShareInfoBasic = New-Object -TypeName "ObjShareInfoBasic"
-    $ObjShareInfoBasic | Add-Member -MemberType NoteProperty -Name 'ShareName' -Value $s.Name
-    $ObjShareInfoBasic | Add-Member -MemberType NoteProperty -Name 'LocalPath' -Value $s.Path
+    $ObjShareInfoBasic = [PSCustomObject]@{
+        ShareName = $s.Name
+        LocalPath = $s.Path
+    }
 }
