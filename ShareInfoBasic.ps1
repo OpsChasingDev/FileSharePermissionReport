f560cea0a9bcfb283c00f39959443f1174371fbb
drummermanrob20 <robert@stapleton-family.net>
2022-06-16 08:39:15 -0400

    custom obj name

--- ShareInfoBasic.ps1
+++ ShareInfoBasic.ps1
@@ -2,6 +2,7 @@ $Share = Get-SmbShare | Where-Object {$_.Name -notlike '*$'}
 $ColShareInfoBasic = @()
 foreach ($s in $Share) {
     $ObjShareInfoBasic = [PSCustomObject]@{
+        PSTypeName = "ObjShareInfoBasic"
         ShareName = $s.Name
         LocalPath = $s.Path
     }
