d84e9514fb3fc9a229b699bb5a2a6b2b7ba9b976
drummermanrob20 <robert@stapleton-family.net>
2022-06-20 14:38:04 -0400

    mandatory param and formatting

--- FSPR_SMBInfoACL.ps1
+++ FSPR_SMBInfoACL.ps1
@@ -2,8 +2,9 @@ function FSPR_SMBInfoACL {
     [OutputType('FSPR.ObjSMBInfoACL')]
     [CmdletBinding()]
     param (
-        [Parameter(ValueFromPipeline)]
-        [ValidateScript({$_.PSObject.TypeNames -eq 'FSPR.ObjShareInfoBasic'})]
+        [Parameter(ValueFromPipeline,
+            Mandatory)]
+        [ValidateScript({ $_.PSObject.TypeNames -eq 'FSPR.ObjShareInfoBasic' })]
         [System.Object]$ShareInfo
     )
     BEGIN {}
@@ -11,12 +12,12 @@ function FSPR_SMBInfoACL {
         $SMBACL = Get-SmbShareAccess -Name $ShareInfo.ShareName
         foreach ($acl in $SMBACL) {
             $obj = [PSCustomObject]@{
-                PSTypeName = "FSPR.ObjSMBInfoACL"
-                ShareName = $ShareInfo.Sharename
-                ShareLocalPath = $ShareInfo.LocalPath
-                AccountName = $acl.AccountName
+                PSTypeName        = "FSPR.ObjSMBInfoACL"
+                ShareName         = $ShareInfo.Sharename
+                ShareLocalPath    = $ShareInfo.LocalPath
+                AccountName       = $acl.AccountName
                 AccessControlType = $acl.AccessControlType
-                AccessRight = $acl.AccessRight
+                AccessRight       = $acl.AccessRight
             }
             Write-Output $obj
         }
