b0c55560cb8b39d3c05810cf26b6cd4f75bf1d7a
drummermanrob20 <robert@stapleton-family.net>
2022-06-21 09:43:09 -0400

    cbh

--- FSPR_NTFSInfoACL.ps1
+++ FSPR_NTFSInfoACL.ps1
@@ -1,4 +1,18 @@
 function FSPR_NTFSInfoACL {
+    <#
+    .SYNOPSIS
+        Gets NTFS permissions information about local paths belonging to non-system SMB Shares on a system.
+    .DESCRIPTION
+        Gets NTFS permissions information about local paths belonging to non-system SMB Shares on a system.
+
+        The purpose of this fucntion is for use in the FilesharePermissionReport whereby the input used for this function is the output generated from the FSPR_ShareInfoBasic function.
+    .LINK
+        https://github.com/OpsChasingDev/FileSharePermissionReport
+    .EXAMPLE
+        PS C:\> FSPR_ShareInfoBasic | FSPR_NTFSInfoACL
+
+        Takes the output FSPR_ShareInfoBasic and feeds this as input to FSPR_SMBInfoACL.  The output generated is a unique object containing information about each NTFS permission for each local path belonging to a non-system SMB Share.
+    #>
     [OutputType('FSPR.ObjNTFSInfoACL')]
     [CmdletBinding()]
     param (
