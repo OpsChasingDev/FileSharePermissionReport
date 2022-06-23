823197d1bba6b6dfd19c249e1f1daa2b565818a8
drummermanrob20 <robert@stapleton-family.net>
2022-06-23 08:18:02 -0400

    remove module files before closing session

--- Constructor.ps1
+++ Constructor.ps1
@@ -1,11 +1,11 @@
-# get data returned for use
-# clean up and remove script files on remote machines
 
 # open new session to remote machine
 $Session = New-PSSession -ComputerName 'SL-FP-01'
 
 # copy module file to remote machine
-Invoke-Command -Session $Session {New-Item -ItemType Directory -Path 'C:\Program Files\WindowsPowerShell\Modules\FSPR\'}
+Invoke-Command -Session $Session {
+    New-Item -ItemType Directory -Path 'C:\Program Files\WindowsPowerShell\Modules\FSPR\'
+}
 $CopySplat = @{
     Path        = 'C:\git\FileSharePermissionReport\FSPR.psm1'
     Destination = 'C:\Program Files\WindowsPowerShell\Modules\FSPR\FSPR.psm1'
@@ -18,6 +18,11 @@ Copy-Item @CopySplat
 Invoke-Command -Session $Session {
     FSPR_ShareInfoBasic | FSPR_SMBInfoACL
     FSPR_ShareInfoBasic | FSPR_NTFSInfoACL
+}
+
+# clean up and remove script files on remote machines
+Invoke-Command -Session $Session {
+    Remove-Item -Path 'C:\Program Files\WindowsPowerShell\Modules\FSPR' -Recurse -Force
 }
 
 # close session
