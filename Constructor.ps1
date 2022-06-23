9ed1e9e1b7f7b1098ca018e17a3b0018ba822f5b
drummermanrob20 <robert@stapleton-family.net>
2022-06-23 08:02:12 -0400

    modified comments

--- Constructor.ps1
+++ Constructor.ps1
@@ -1,11 +1,11 @@
-# constructor script intended to be invoked to other machines for processing their share data permissions
-
-# copy all script files to the remote machine
 # execute script logic on remote machine
 # get data returned for use
 # clean up and remove script files on remote machines
 
+# open new session to remote machine
 $Session = New-PSSession -ComputerName 'SL-FP-01'
+
+# copy module file to remote machine
 Invoke-Command -Session $Session {New-Item -ItemType Directory -Path 'C:\Program Files\WindowsPowerShell\Modules\FSPR\'}
 $CopySplat = @{
     Path        = 'C:\git\FileSharePermissionReport\FSPR.psm1'
