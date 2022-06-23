35b0801c5dcbe6c6dab5c1e6a45cf9162f0ca64e
drummermanrob20 <robert@stapleton-family.net>
2022-06-23 08:14:54 -0400

    invocation with module file move, logic, and session removal

--- Constructor.ps1
+++ Constructor.ps1
@@ -1,4 +1,3 @@
-# execute script logic on remote machine
 # get data returned for use
 # clean up and remove script files on remote machines
 
@@ -14,3 +13,12 @@ $CopySplat = @{
     ToSession   = $Session
 }
 Copy-Item @CopySplat
+
+# execute script logic on remote machine
+Invoke-Command -Session $Session {
+    FSPR_ShareInfoBasic | FSPR_SMBInfoACL
+    FSPR_ShareInfoBasic | FSPR_NTFSInfoACL
+}
+
+# close session
+Remove-PSSession -Session $Session
