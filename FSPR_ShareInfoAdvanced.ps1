ce6a34818e553a5b3ee0219305fe394e6008712c
drummermanrob20 <robert@stapleton-family.net>
2022-06-27 08:34:03 -0400

    set param and cleared process block

--- FSPR_ShareInfoAdvanced.ps1
+++ FSPR_ShareInfoAdvanced.ps1
@@ -5,23 +5,13 @@ function FSPR_NTFSInfoACL {
         [Parameter(ValueFromPipeline,
             Mandatory)]
         [ValidateScript({ $_.PSObject.TypeNames -eq 'FSPR.ObjShareInfoBasic' })]
-        [System.Object]$NTFSInfo
+        [System.Object]$ShareInfo
     )
 
     BEGIN {}
 
     PROCESS {
-        $NTFSACL = Get-ACL -Path $NTFSInfo.LocalPath
-        foreach ($acl in $NTFSACL.Access) {
-            $obj = [PSCustomObject]@{
-                PSTypeName        = "FSPR.ObjNTFSInfoACL"
-                ShareLocalPath    = $NTFSInfo.LocalPath
-                AccountName       = $acl.IdentityReference
-                AccessControlType = $acl.AccessControlType
-                AccessRight       = $acl.FileSystemRights
-            }
-            Write-Output $obj
-        }
+    
     }
 
     END {}
