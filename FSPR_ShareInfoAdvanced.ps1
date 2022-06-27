cb22b14cca0099f4bb37f1bc816f01c6f6e00cd6
drummermanrob20 <robert@stapleton-family.net>
2022-06-27 08:30:40 -0400

    changed output type

--- FSPR_ShareInfoAdvanced.ps1
+++ FSPR_ShareInfoAdvanced.ps1
@@ -1,1 +1,28 @@
-FSPR_ShareInfoAdvanced.ps1
+function FSPR_NTFSInfoACL {
+    [OutputType('FSPR.ObjShareInfoAdvanced')]
+    [CmdletBinding()]
+    param (
+        [Parameter(ValueFromPipeline,
+            Mandatory)]
+        [ValidateScript({ $_.PSObject.TypeNames -eq 'FSPR.ObjShareInfoBasic' })]
+        [System.Object]$NTFSInfo
+    )
+
+    BEGIN {}
+
+    PROCESS {
+        $NTFSACL = Get-ACL -Path $NTFSInfo.LocalPath
+        foreach ($acl in $NTFSACL.Access) {
+            $obj = [PSCustomObject]@{
+                PSTypeName        = "FSPR.ObjNTFSInfoACL"
+                ShareLocalPath    = $NTFSInfo.LocalPath
+                AccountName       = $acl.IdentityReference
+                AccessControlType = $acl.AccessControlType
+                AccessRight       = $acl.FileSystemRights
+            }
+            Write-Output $obj
+        }
+    }
+
+    END {}
+}
