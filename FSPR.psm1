2bc9b26760332be9dd707ddd82c28a2413635dac
drummermanrob20 <robert@stapleton-family.net>
2022-06-27 10:07:06 -0400

    moved FSPR_ShareInfoAdvanced into FSPR.psm1

--- FSPR.psm1
+++ FSPR.psm1
@@ -143,3 +143,37 @@ function FSPR_NTFSInfoACL {
 
     END {}
 }
+
+function FSPR_ShareInfoAdvanced {
+    [OutputType('FSPR.ObjShareInfoAdvanced')]
+    [CmdletBinding()]
+    param (
+        [Parameter(ValueFromPipeline,
+            Mandatory)]
+        [ValidateScript({ $_.PSObject.TypeNames -eq 'FSPR.ObjShareInfoBasic' })]
+        [System.Object]$ShareInfo,
+
+        [Parameter(Mandatory)]
+        [ValidateScript({ $_.PSObject.TypeNames -eq 'FSPR.ObjSMBInfoACL' })]
+        [System.Object]$SMBInfoACL,
+
+        [Parameter(Mandatory)]
+        [ValidateScript({ $_.PSObject.TypeNames -eq 'FSPR.ObjNTFSInfoACL' })]
+        [System.Object]$NTFSInfoACL
+    )
+
+    BEGIN {}
+
+    PROCESS {
+        $obj = [PSCustomObject]@{
+            PSTypeName = 'FSPR.ObjShareInfoAdvanced'
+            ComputerName = $env:COMPUTERNAME
+            ShareName = $_.ShareName
+            LocalPath = $_.LocalPath
+            Permission = $Permission
+        }
+        Write-Output $obj
+    }
+
+    END {}
+}
