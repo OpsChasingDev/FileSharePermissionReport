5cf16b6c3395e1ce94eb300fc4c5ab661c27e604
drummermanrob20 <robert@stapleton-family.net>
2022-06-28 09:38:08 -0400

    adjust variable names

--- pseudo-code.ps1
+++ pseudo-code.ps1
@@ -1,18 +1,18 @@
 foreach ($Share in $ShareCollection) {
     # take the share name and iterate through NTFS and SMB permissions objects where the local paths match
     # if they match, add the ACL properties of that object to the respective object member
-    $SMBCollection = @()
-    foreach ($SMB in $SMBACLs) {
+    $SMB_Collection = @()
+    foreach ($SMB in $SMB_ACL_Collection) {
         if ($SMB.LocalPath -eq $Share.LocalPath) {
             $SMBobj = [PSCustomObject]@{
                 AccountName = $SMB.AccountName
                 AccessControlType = $SMB.AccessControlType
                 AccessRight = $SMB.AccessRight
             }
-        $SMBCollection += $SMBobj
+        $SMB_Collection += $SMBobj
         }
     }
-    # add the $SMBCollection value as a member of the $Share psobject
+    # add the $SMB_Collection value as a member of the $Share psobject
 }
 
 <#
