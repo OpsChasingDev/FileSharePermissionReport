d8deb0742221957d78d95919daf64a8f2994a99a
drummermanrob20 <robert@stapleton-family.net>
2022-06-28 09:36:02 -0400

    smb logic

--- pseudo-code.ps1
+++ pseudo-code.ps1
@@ -1,6 +1,18 @@
 foreach ($Share in $ShareCollection) {
     # take the share name and iterate through NTFS and SMB permissions objects where the local paths match
-    # if they match, add the ACL properties of that object to the 
+    # if they match, add the ACL properties of that object to the respective object member
+    $SMBCollection = @()
+    foreach ($SMB in $SMBACLs) {
+        if ($SMB.LocalPath -eq $Share.LocalPath) {
+            $SMBobj = [PSCustomObject]@{
+                AccountName = $SMB.AccountName
+                AccessControlType = $SMB.AccessControlType
+                AccessRight = $SMB.AccessRight
+            }
+        $SMBCollection += $SMBobj
+        }
+    }
+    # add the $SMBCollection value as a member of the $Share psobject
 }
 
 <#
